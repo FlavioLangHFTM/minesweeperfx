3ee8cb6e22227d11475b7bad680ecc22fa02f0ca
Flavio Lang <flavio.lang@hftm.ch>
2023-05-22 18:39:07 +0200

    b) Removed unnecessary components from base project

---
+++ src/main/java/ch/hftm/MainScreenController.java
@@ -0,0 +1,12 @@
+package ch.hftm;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class MainScreenController {
+
+    @FXML
+    private void hello() throws IOException {
+        System.out.println("Hello World!!! This is my JavaFX Applicatio!");
+    }
+}
